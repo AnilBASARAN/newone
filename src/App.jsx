8f8adef85a8fc7c415fc4f5e0457c3eb65cf59f4
AnilBASARAN <nlbsrn@gmail.com>
2025-03-23 00:45:32 +0300

    we are using react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import "./App.css";
 
-
-import './App.css'
+const getUsers = () => axios.get("http://localhost:3004/users").then((res) => res.data);
 
 function App() {
+  const {data:users=[]} = useQuery({
+    queryKey: ["users"], // ✅ Object-based syntax (required in v5)
+    queryFn: getUsers,
+  });
 
-  return (
-   <div>
-    App
-  
-   </div>
-  )
+  console.log(users);
+
+  return <div>
+    <div>
+    Total: {users.length}
+    </div>
+  <div>{users.map(user=>(
+    <div key={user.id}>{user.name}</div>
+  ))}</div>
+
+  </div>;
 }
 
-export default App
+export default App;
